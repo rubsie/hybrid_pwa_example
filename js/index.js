7f290d1b037b66b3ead29e765866900322d2e886
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-01-04 17:06:16 +0100

    version 4: dress-up

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
 console.log("run index.js");
 
+
+const POKEMON_DATA = [
+    {id: 113, name: "chansey", type: "normal"},
+    {id: 202, name: "wobbuffet", type: "psychic"},
+    {id: 289, name: "slaking", type: "normal"},
+    {id: 376, name: "metagross", type: "steel/psychic"},
+    {id: 862, name: "obstagoon", type: "dark/normal"}];
+
+const alertElem = document.getElementById("alert_div");
+const imgElem = document.getElementById("pokemon_img");
+const pokemonIdElem = document.getElementById("pokemon_id");
+const pokemonNameElem = document.getElementById("pokemon_name");
+const typeElem = document.getElementById("pokemon_type");
+
+
+let currentPokemonIndex = -1;
+showNextPokemon();
+
+function showNextPokemon() {
+    currentPokemonIndex = currentPokemonIndex < POKEMON_DATA.length - 1 ? currentPokemonIndex + 1 : 0;
+    const pokemonToShow = POKEMON_DATA[currentPokemonIndex];
+    imgElem.src = `images/${pokemonToShow.id}.png`;
+    pokemonIdElem.innerText = pokemonToShow.id;
+    pokemonNameElem.innerText = pokemonToShow.name;
+    typeElem.innerText = pokemonToShow.type;
+}
+
+
 // Register service worker to control making site work offline
 async function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
